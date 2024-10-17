1f3c8f28860052302addf2ee997e134f863f8ddc
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-17 14:46:06 +0500

    completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
 import NavBar from "./component/NavBar";
 
 const geistSans = localFont({
